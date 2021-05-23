1b2c3f1dbaac1aa64bcc7cfb9a44051ebbeb9c81
Sunil Bidgar <s.bidgar@gmail.com>
2021-05-23 17:45:46 +0530

    Code chanes

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
 import sys
 
-# import requests
+import requests
 
 # sys.stdout.write("Python %s\n" % (sys.version,))
 # print(sys.path)
 print(sys.executable)
+
+r = requests.get(url="https://google.com", verify=False)
+print(r.status_code)
+print(r.ok)
